6f30f50c884bccbf6f6394153859c8bab94fdcab
Vladhus <49909976+Vladhus@users.noreply.github.com>
2020-11-26 19:27:33 +0100

    Login and Register in Angular UI added

--- DatingAPI/Controllers/UsersController.cs
+++ DatingAPI/Controllers/UsersController.cs
@@ -20,14 +20,12 @@ namespace DatingAPI.Controllers
 
 
         [HttpGet]
-        [Authorize]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        [Authorize]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             return await _context.Users.FindAsync(id);
